f95c3ce890adc5a2b04ead9e1f42212923f6b48c
Diaz <biksultanovdiaz@gmail.com>
2023-04-02 12:12:21 +0600

    First Commit

---
+++ tsis6/Dir and files/ab.py
@@ -0,0 +1,13 @@
+import os
+import re
+
+open(data.txt(C:\Users\Acer\OneDrive\Documents\MyGit\tsis6\Dir and files))
+read(data.txt)  
+def abtoba(text)
+    patterns ('^ab $+' )
+    if abtoba.match (open(C:\Users\Acer\OneDrive\Documents\MyGit\tsis6\Dir and files))
+        return ('^ba$+' in data.txt)
+
+print(data.txt)
+
+
---
+++ tsis6/Dir and files/test.py
@@ -0,0 +1,5 @@
+import os
+
+def generator()
+    generator (open("data.txt", "r"))
+print(f.readline())
